8cf34a078eb38c3beb4b2303af4fe5cbf2e6a812
hongyeol <drrd100@naver.com>
2022-04-15 16:26:54 +0900

    add GoStore

--- src/components/GoStore.jsx
+++ src/components/GoStore.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function GoStore() {
+    const goStore = useRef();
+
+    function createScrollStopListener(element, callback, timeout) {
+        let handle = null;
+        let onScroll = function() {
+            if (handle) {
+                clearTimeout(handle);
+            }
+            handle = setTimeout(callback, timeout || 200); 
+        };
+        element.addEventListener('scroll', onScroll);
+        return function() {
+            element.removeEventListener('scroll', onScroll);
+        };
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll" , () => { 
+            goStore.current.classList.remove("show-store")
+        })
+        createScrollStopListener(window, function() {
+            goStore.current.classList.add("show-store")
+        });
+    
+    },[])
+    
+    
+    
   return (
     <>
-        <div className='go-store'>
+        <div className='go-store' ref={goStore}>
             <Link to="/">
                 <img src="/images/logo-store.png" alt="" />
                 <span>앱 다운로드</span>
